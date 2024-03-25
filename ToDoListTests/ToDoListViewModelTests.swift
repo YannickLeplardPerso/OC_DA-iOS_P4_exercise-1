6da33ce2ca60fce843643d912e54c7203c26e6b4
Yannick Leplard <leplard.yannick@gmail.com>
2024-03-25 17:26:21 +0100

    modification testFilteredToDoItems (meilleure couverture)

--- ToDoListTests/ToDoListViewModelTests.swift
+++ ToDoListTests/ToDoListViewModelTests.swift
@@ -66,23 +66,31 @@ final class ToDoListViewModelTests: XCTestCase {
         // Given
         let item1 = ToDoItem(title: "Task 1", isDone: true)
         let item2 = ToDoItem(title: "Task 2", isDone: false)
+        let item3 = ToDoItem(title: "Task 3", isDone: false)
+        let item4 = ToDoItem(title: "Task 4", isDone: true)
+        let item5 = ToDoItem(title: "Task 5", isDone: false)
         viewModel.add(item: item1)
         viewModel.add(item: item2)
+        viewModel.add(item: item3)
+        viewModel.add(item: item4)
+        viewModel.add(item: item5)
         
         // When
         let itemsAll = viewModel.applyFilter(at: .all)
         // Then
-        XCTAssertEqual(itemsAll.count, 2)
+        XCTAssertEqual(itemsAll.count, 5)
         
         // When
         let itemsDone = viewModel.applyFilter(at: .done)
         // Then
-        XCTAssertEqual(itemsDone.count, 1)
+        XCTAssertEqual(itemsDone.count, 2)
+        XCTAssertEqual(itemsDone.first?.title, item1.title)
         
         // When
         let itemsNotDone = viewModel.applyFilter(at: .notDone)
         // Then
-        XCTAssertEqual(itemsNotDone.count, 1)
+        XCTAssertEqual(itemsNotDone.count, 3)
+        XCTAssertEqual(itemsNotDone.first?.title, item2.title)
         
     }
 }
